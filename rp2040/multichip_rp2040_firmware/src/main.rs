6c7d5ea0a5c0fb82553b48558ac2298d9023d62d
Matthew Baggett <matthew@baggett.me>
2024-04-09 17:40:49 +0200

    For now disable all the blinkenlights.

--- rp2040/multichip_rp2040_firmware/src/main.rs
+++ rp2040/multichip_rp2040_firmware/src/main.rs
@@ -1,33 +1,31 @@
 #![no_std]
 #![no_main]
 
-
 use cyw43::Control;
 use cyw43_pio::PioSpi;
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_futures::join::{join,join3};
-use embassy_rp::{bind_interrupts};
-use embassy_rp::gpio::{Level, Input, Output, AnyPin, Pin, Pull};
+use embassy_futures::join::{join, join3};
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{AnyPin, Input, Level, Output, Pin, Pull};
 use embassy_rp::peripherals::{DMA_CH0, PIO0, USB};
-use embassy_rp::pio::{Pio};
+use embassy_rp::pio::Pio;
 use embassy_rp::usb::{Driver, Instance};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::{Channel, Sender};
 use embassy_sync::mutex::Mutex;
+use embassy_time::{Duration, Ticker, Timer};
 use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
 use embassy_usb::driver::EndpointError;
 use embassy_usb_logger;
-use embassy_sync::channel::{Channel, Sender};
-use embassy_time::{Duration, Ticker, Timer};
-use static_cell::StaticCell;
-use {defmt_rtt as _, panic_probe as _};
 use log::log;
 use rp2040_boot2;
+use static_cell::StaticCell;
+use {defmt_rtt as _, panic_probe as _};
 
 #[link_section = ".boot_loader"]
 #[used]
-pub static BOOT_LOADER: [u8; 16384] =
-    *include_bytes!("../firmware/serial-bootloader.bin");
+pub static BOOT_LOADER: [u8; 16384] = *include_bytes!("../firmware/serial-bootloader.bin");
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => embassy_rp::pio::InterruptHandler<PIO0>;
@@ -35,7 +33,9 @@ bind_interrupts!(struct Irqs {
 });
 
 #[embassy_executor::task]
-async fn wifi_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
+async fn wifi_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
     runner.run().await
 }
 
@@ -105,10 +105,9 @@ async fn main(spawner: Spawner) {
         *(BACKLIGHT.lock().await) = Some(backlight);
     }
 
-
     let jogball_fut = async {
         log::info!("Awaiting Jogball changes");
-        loop{
+        loop {
             match CHANNEL_JOGBALL.receive().await {
                 JogBall::CLICK => log::info!("Jogball CLICK"),
                 JogBall::UP => log::info!("Jogball Up"),
@@ -132,7 +131,15 @@ async fn main(spawner: Spawner) {
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
     let mut pio = Pio::new(p.PIO0, Irqs);
-    let spi = PioSpi::new(&mut pio.common, pio.sm0, pio.irq0, cs, p.PIN_24, p.PIN_29, p.DMA_CH0);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
 
     static STATE: StaticCell<cyw43::State> = StaticCell::new();
     let state = STATE.init(cyw43::State::new());
@@ -186,7 +193,8 @@ async fn main(spawner: Spawner) {
 
     // Creates the logger and returns the logger future
     // Note: You'll need to use log::info! afterwards instead of info! for this to work (this also applies to all the other log::* macros)
-    let log_fut = embassy_usb_logger::with_class!(1024, log::LevelFilter::Info, serial_logger_acm_class);
+    let log_fut =
+        embassy_usb_logger::with_class!(1024, log::LevelFilter::Info, serial_logger_acm_class);
 
     // Build the builder.
     let mut usb = builder.build();
@@ -212,43 +220,70 @@ async fn main(spawner: Spawner) {
     };
 
     // Led Tasks
-    unwrap!(spawner.spawn(toggle_led(&WHITE_LED,Duration::from_hz(10))));
-    unwrap!(spawner.spawn(toggle_led(&GREEN_LED,Duration::from_hz(9))));
-    unwrap!(spawner.spawn(toggle_led(&RED_LED,Duration::from_hz(8))));
-    unwrap!(spawner.spawn(toggle_led(&BLUE_LED,Duration::from_hz(11))));
-    unwrap!(spawner.spawn(toggle_led(&BACKLIGHT,Duration::from_hz(15))));
+    //unwrap!(spawner.spawn(toggle_led(&WHITE_LED,Duration::from_hz(10))));
+    //unwrap!(spawner.spawn(toggle_led(&GREEN_LED,Duration::from_hz(9))));
+    //unwrap!(spawner.spawn(toggle_led(&RED_LED,Duration::from_hz(8))));
+    //unwrap!(spawner.spawn(toggle_led(&BLUE_LED,Duration::from_hz(11))));
+    //unwrap!(spawner.spawn(toggle_led(&BACKLIGHT,Duration::from_hz(15))));
 
     // Jogball tasks
-    unwrap!(spawner.spawn(jogball(&JOGBALL_UP,    CHANNEL_JOGBALL.sender(), JogBall::UP)));
-    unwrap!(spawner.spawn(jogball(&JOGBALL_DOWN,  CHANNEL_JOGBALL.sender(), JogBall::DOWN)));
-    unwrap!(spawner.spawn(jogball(&JOGBALL_LEFT,  CHANNEL_JOGBALL.sender(), JogBall::LEFT)));
-    unwrap!(spawner.spawn(jogball(&JOGBALL_RIGHT, CHANNEL_JOGBALL.sender(), JogBall::RIGHT)));
-    unwrap!(spawner.spawn(click(&JOGBALL_CLICK,   CHANNEL_JOGBALL.sender(), JogBall::CLICK)));
+    unwrap!(spawner.spawn(jogball(&JOGBALL_UP, CHANNEL_JOGBALL.sender(), JogBall::UP)));
+    unwrap!(spawner.spawn(jogball(
+        &JOGBALL_DOWN,
+        CHANNEL_JOGBALL.sender(),
+        JogBall::DOWN
+    )));
+    unwrap!(spawner.spawn(jogball(
+        &JOGBALL_LEFT,
+        CHANNEL_JOGBALL.sender(),
+        JogBall::LEFT
+    )));
+    unwrap!(spawner.spawn(jogball(
+        &JOGBALL_RIGHT,
+        CHANNEL_JOGBALL.sender(),
+        JogBall::RIGHT
+    )));
+    unwrap!(spawner.spawn(click(
+        &JOGBALL_CLICK,
+        CHANNEL_JOGBALL.sender(),
+        JogBall::CLICK
+    )));
 
     log::info!("Starting up MultiChip on RP2040!");
     // Run everything concurrently.
     // If we had made everything `'static` above instead, we could do this using separate tasks instead.
     join3(
         jogball_fut,
-        join(blinken_lights_future,usb_fut),
-        join(echo_fut, log_fut)
-    ).await;
+        join(blinken_lights_future, usb_fut),
+        join(echo_fut, log_fut),
+    )
+    .await;
 }
 
 #[embassy_executor::task(pool_size = 4)]
-async fn jogball(jogball_btn: &'static JogballButtonType, control: Sender<'static, ThreadModeRawMutex, JogBall,64>, direction: JogBall){
-
+async fn jogball(
+    jogball_btn: &'static JogballButtonType,
+    control: Sender<'static, ThreadModeRawMutex, JogBall, 64>,
+    direction: JogBall,
+) {
     loop {
-        let mut jogball_btn_unlocked  = jogball_btn.lock().await;
-        jogball_btn_unlocked.as_mut().unwrap().wait_for_any_edge().await;
+        let mut jogball_btn_unlocked = jogball_btn.lock().await;
+        jogball_btn_unlocked
+            .as_mut()
+            .unwrap()
+            .wait_for_any_edge()
+            .await;
         control.send(direction).await;
     }
 }
 #[embassy_executor::task]
-async fn click(btn: &'static ButtonType, control: Sender<'static, ThreadModeRawMutex, JogBall,64>, direction: JogBall ){
-
+async fn click(
+    btn: &'static ButtonType,
+    control: Sender<'static, ThreadModeRawMutex, JogBall, 64>,
+    direction: JogBall,
+) {
     loop {
-        let mut btn_unlocked  = btn.lock().await;
+        let mut btn_unlocked = btn.lock().await;
         btn_unlocked.as_mut().unwrap().wait_for_falling_edge().await;
         control.send(direction).await;
     }
@@ -281,7 +316,9 @@ impl From<EndpointError> for Disconnected {
     }
 }
 
-async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) -> Result<(), Disconnected> {
+async fn echo<'d, T: Instance + 'd>(
+    class: &mut CdcAcmClass<'d, Driver<'d, T>>,
+) -> Result<(), Disconnected> {
     let mut buf = [0; 64];
     info!("Entered echo async");
     loop {
@@ -294,4 +331,3 @@ async fn echo<'d, T: Instance + 'd>(class: &mut CdcAcmClass<'d, Driver<'d, T>>) 
         }
     }
 }
-
